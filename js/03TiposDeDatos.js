379db605f1bfb3b812cc17892a037043d93ce5b6
Ehylin <ehylinolezjua@.com>
2022-02-22 10:29:43 -0300

    tipos de datos

---
+++ js/03TiposDeDatos.js
@@ -0,0 +1,44 @@
+//Tipos de datos
+
+//Undefined - se le asigna a una variable que no tiene dato, que solo esta creada
+
+let cliente 
+console.log(cliente)
+console.log(typeof cliente)
+
+
+// cuando se le asigna un valor
+
+let cliente2 = 20
+console.log(cliente2)
+console.log(typeof cliente2)
+
+//Boolean
+
+const descuento = false
+console.log(descuento)
+console.log(typeof descuento)
+
+//Number - todos van hacer numeros
+
+const numero1 = 20.20
+const numero2 = 30
+const numero3 = -100
+
+console.log(typeof numero1)
+console.log(typeof numero2)
+console.log(typeof numero3)
+
+//String o cadena de texto
+const alumno = "Ehylin"
+let producto = 'Monitor 20 pulgadas'
+
+console.log(alumno)
+console.log(producto)
+
+const numeroString = "30"
+const numeroNumber = 30
+
+console.log(typeof numeroString)
+console.log(typeof numeroNumber)
+
